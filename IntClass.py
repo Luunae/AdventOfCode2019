5f13ca3339e257bec2701bb6ebe8019473fd2f68
Luna Nova <49705655+Luunae@users.noreply.github.com>
2019-12-13 17:18:26 +0000

    Day 5 Final

--- IntClass.py
+++ IntClass.py
@@ -1,17 +1,28 @@
 import csv
-from enum import Enum
+from enum import IntEnum
 
 
-class Mode(Enum):
+class Mode(IntEnum):
     POSITION = 0
     IMMEDIATE = 1
+
+
+class Opcode(IntEnum):
+    ADD = 1
+    MULTIPLY = 2
+    INPUT = 3
+    OUTPUT = 4
+    JUMPIFTRUE = 5
+    JUMPIFFALSE = 6
+    LESSTHAN = 7
+    EQUAL = 8
 
 
 class Intcore:
     def __init__(self, file):
         self.file = file
         self.i = 0
-        self.opcodes = [1, 2, 3, 4, 99]
+        self.opcodes = [1, 2, 3, 4, 5, 6, 7, 8, 99]
         self.av = []
         self.point = 0
         self.mode_a = Mode.POSITION
@@ -23,18 +34,30 @@ class Intcore:
 
     def go(self):
         self.load(self.file)
+        print(len(self.av))
         while (self.av[self.point] % 100) != 99:
             self.getmode()
             opcode = self.av[self.point] % 100
             if opcode in self.opcodes:
-                if opcode == self.opcodes[0]:
+                if opcode == Opcode.ADD:
                     self.add(self.point, self.modelist)
-                elif opcode == self.opcodes[1]:
+                elif opcode == Opcode.MULTIPLY:
                     self.mult(self.point, self.modelist)
-                elif opcode == self.opcodes[2]:
+                elif opcode == Opcode.INPUT:
                     self.store(self.point, self.modelist)
-                else:
+                elif opcode == Opcode.OUTPUT:
                     self.pull(self.point, self.modelist)
+                elif opcode == Opcode.JUMPIFTRUE:
+                    self.jumpiftrue(self.point, self.modelist, True)
+                elif opcode == Opcode.JUMPIFFALSE:
+                    self.jumpiftrue(self.point, self.modelist, False)
+                elif opcode == Opcode.LESSTHAN:
+                    self.lessthanequals(self.point, self.modelist, True)
+                elif opcode == Opcode.EQUAL:
+                    self.lessthanequals(self.point, self.modelist, False)
+                else:
+                    print("fuck")
+                    break
             else:
                 print("fuck")
                 print(self.av)
@@ -120,3 +143,45 @@ class Intcore:
             output = self.av[point + 1]
         print("\t\tOUT\t", output)
         self.point += 2
+
+    def jumpiftrue(self, point, modelist, to_true):
+        if modelist[0] == Mode.POSITION:
+            check_value = self.av[self.av[point + 1]]
+        else:
+            check_value = self.av[point + 1]
+        result = False
+        if to_true:
+            if check_value != 0:
+                result = True
+        else:
+            if check_value == 0:
+                result = True
+        if result:
+            if modelist[1] == Mode.POSITION:
+                self.point = self.av[self.av[point + 2]]
+            else:
+                self.point = self.av[point + 2]
+        else:
+            self.point += 3
+
+    def lessthanequals(self, point, modelist, lessthan):
+        if modelist[0] == Mode.POSITION:
+            firstnum = self.av[self.av[point + 1]]
+        else:
+            firstnum = self.av[point + 1]
+        if modelist[1] == Mode.POSITION:
+            secondnum = self.av[self.av[point + 2]]
+        else:
+            secondnum = self.av[point + 2]
+        result = False
+        if lessthan:
+            if firstnum < secondnum:
+                result = True
+        else:
+            if firstnum == secondnum:
+                result = True
+        if result:
+            self.av[self.av[point + 3]] = 1
+        else:
+            self.av[self.av[point + 3]] = 0
+        self.point += 4
