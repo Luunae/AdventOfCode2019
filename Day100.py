289641710d7baa82eb029bb53e0ae25c646ba24f
Luna Nova <49705655+Luunae@users.noreply.github.com>
2019-12-21 21:38:30 +0000

    Day 10 pt 1 Unf

--- Day100.py
+++ Day100.py
@@ -1,10 +1,23 @@
-testloc = [0, 0]
-locs = [[0, 0, 0], [2, 2, 0], [1, 1, 0]]
+file = "test.txt"
+
+def load(file):
+    locs = []
+    with open(file, 'r') as inputfile:
+        y = 0
+        for line in inputfile:
+            x = 0
+            for char in line.strip():
+                if char == '#':
+                    locs.append([x, y, 0])
+                x += 1
+            y += 1
+
+    return locs
 
 
 def pathcheck(posa, posb, locs):
-    posax, posay = posa
-    posbx, posby = posb
+    posax, posay, asteroids = posa
+    posbx, posby, asteroids = posb
     minx = min(posax, posbx)
     miny = min(posay, posby)
     maxx = max(posax, posbx)
@@ -12,32 +25,52 @@ def pathcheck(posa, posb, locs):
 
     ydist = posay - posby
     xdist = posax - posbx
-    dist = ((ydist**2) + (xdist**2))**0.5
-    print("posa: ", posa)
-    print("posb: ", posb)
-    print("posax: ", posax)
-    print("posay: ", posay)
-    print("posbx: ", posbx)
-    print("posby: ", posby)
-    print("ydist: ", ydist)
-    print("xdist: ", xdist)
-    print("dist: ", dist)
 
-    # y = m*x + b
-    gradient = ydist / xdist
+    if xdist != 0:
+        gradient = ydist / xdist
+    else:
+        gradient = float("inf")
 
-    print(gradient)
     elevation = posay - posax * gradient
-    print(elevation)
     for loc in locs:
         if loc == posa or loc == posb:
             continue
-        x, y = loc
-        x_should = (y - elevation) / gradient
-        if x == x_should and minx < x < maxx and miny < y < maxy:
-            return False
+        x, y, asteroids = loc
+
+        if not (minx <= x <= maxx and miny <= y <= maxy):
+            continue
+
+        if gradient == float("inf"):
+            if posax == x:
+                return False
+
+        elif gradient != 0:
+            x_should = (y - elevation) / gradient
+            if x == x_should:
+                return False
+        else:
+            if posay == y:
+                return False
+
     return True
 
 
+locs = load(file)
+print(locs)
 for i in range(0, len(locs)):
     for j in range(0, len(locs)):
+        if i == j:
+            continue
+        if pathcheck(locs[i], locs[j], locs):
+            locs[i][2] += 1
+
+print("locs: ", locs)
+
+maxct = 0
+bestloc = None
+for loc in locs:
+    if loc[2] > maxct:
+        maxct = loc[2]
+        bestloc = loc
+print("best loc: ", bestloc)
+print("maxct: ", maxct)
