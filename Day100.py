a69527ad81fd9287d23114c2bbc0433dc063b0f1
Luna Nova <49705655+Luunae@users.noreply.github.com>
2019-12-22 20:28:15 +0000

    Day 10 pt 1 complete

--- Day100.py
+++ Day100.py
@@ -1,54 +1,57 @@
-file = "test.txt"
+file = "input_10.txt"
 
 def load(file):
-    locs = []
+    asteroidlocations = []
     with open(file, 'r') as inputfile:
         y = 0
         for line in inputfile:
             x = 0
             for char in line.strip():
                 if char == '#':
-                    locs.append([x, y, 0])
+                    asteroidlocations.append([x, y, 0])
                 x += 1
             y += 1
-    return locs
+    return asteroidlocations
 
 
-def pathcheck(posa, posb, locs):
-    posax, posay, asteroids = posa
-    posbx, posby, asteroids = posb
-    minx = min(posax, posbx)
-    miny = min(posay, posby)
-    maxx = max(posax, posbx)
-    maxy = max(posay, posby)
-    ydist = posay - posby
-    xdist = posax - posbx
+def noncollinearitycheck(firstasteroid, secondasteroid, asteroidlist):
+    firstasteroidx, firstasteroidy, _numasteroids = firstasteroid
+    secondasteroidx, secondasteroidy, _numasteroids = secondasteroid
+    minx = min(firstasteroidx, secondasteroidx)  # range setup
+    miny = min(firstasteroidy, secondasteroidy)
+    maxx = max(firstasteroidx, secondasteroidx)
+    maxy = max(firstasteroidy, secondasteroidy)
+    ydist = firstasteroidy - secondasteroidy
+    xdist = firstasteroidx - secondasteroidx
 
+# if xdist (the distance between firstasteroidx and secondasteroidx is zero, the line is vertical.
+# TODO: Better way to do that?
     if xdist != 0:
         gradient = ydist / xdist
     else:
         gradient = float("inf")
 
-    elevation = posay - posax * gradient
-    for loc in locs:
-        if loc == posa or loc == posb:
+# This part checks every asteroid in the list to make sure it's not inbetween posa, posb.
+# SUPER FUCKING INEFFICIENT.
+# TODO: work on algorithm to minimize overhead.
+    for compare in asteroidlist:
+        if compare == firstasteroid or compare == secondasteroid:  # If current asteroid checking is either asteroids...
+            continue                       # Skip. Should only happen twice per loop?
+        comparex, comparey, _numasteroids = compare      # Split out info for use. EZ enough.
+
+        # Range checking. Make sure asteroid isn't behind posa or posb. Should remove false LOS break positives.
+        if not (minx <= comparex <= maxx and miny <= comparey <= maxy):
             continue
-        x, y, asteroids = loc
 
-        if not (minx <= x <= maxx and miny <= y <= maxy):
-            continue
-
-        if gradient == float("inf"):
-            if posax == x:
-                return False
-
-        elif gradient != 0:
-            x_should = (y - elevation) / gradient
-            if x == x_should:
-                return False
+        firstthirdydist = firstasteroidy - comparey
+        firstthirdxdist = firstasteroidx - comparex
+        if firstthirdxdist != 0:
+            firstthirdgradient = firstthirdydist / firstthirdxdist
         else:
-            if posay == y:
-                return False
+            firstthirdgradient = float("inf")
+
+        if firstthirdgradient == gradient:
+            return False
     return True
 
 
@@ -58,7 +61,7 @@ for i in range(0, len(locs)):
     for j in range(0, len(locs)):
         if i == j:
             continue
-        if pathcheck(locs[i], locs[j], locs):
+        if noncollinearitycheck(locs[i], locs[j], locs):
             locs[i][2] += 1
 
 print("locs: ", locs)
